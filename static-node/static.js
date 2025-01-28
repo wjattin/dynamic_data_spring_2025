d1d7dcd07cd03bd2585f842538b328cd1a8ad1d8
Billy Jattin <bjattin@veteranmediagroup.com>
2025-01-28 16:35:09 -0500

    Final Lab 2

--- static-node/static.js
+++ static-node/static.js
@@ -1,5 +1,6 @@
 //variables
 const http = require('http');
+
 const fs = require('fs')
 
 const hostname = '127.0.0.1';
@@ -9,22 +10,58 @@ const port = 3000;
 const someName = (name)=>{
     console.log("Your name is " + name)
 }
+
 someName("William Jattin")
+// Create a function to read files and display them
+const displayPage = (path, r, status = 200)=> {
+  r.setHeader('Content-Type', 'text/html')
+  fs.readFile( path, (error,content)=>{
+      //We need to handle errors first
+      if(error) {
+        r.statusCode = 500
+        r.end("500 - error")
+      } else {
+      r.statusCode = status
+      //if there are no errors, we can output the content
+      r.end(content)
+      }
+  })
+}
+
 
 const server = http.createServer((req, res) => {
-console.log(req.url)  
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-fs.readFile('./public/home.html',(error,content)=>{
-    //We need to handle errors first 
 
-    //if there are no errors, we can output the content
-    res.end(content)
-})
+  console.log(req.url)
+  
+  switch (req.url) {
+    case "":  
+    case "/":
+    //Respond to /
+    displayPage('./public/home.html',res)
+    break;
+    
+    case "/about":
+    //Respond to /
+    displayPage('./public/about.html',res)
+    break;
 
+    case "/contact":
+    //Respond to /
+    displayPage('./public/contact.html',res)
+    break;
+
+    default:
+      //Respond to /
+      displayPage('./public/404.html',res,404)
+
+  }
+
+ 
 
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/ ...Press ctrl + c to close`);
 });
+
+
