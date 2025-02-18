9841b405113754af110e1131f95dc70c9d4cabbf
William Jattin <wjattin@miami.edu>
2025-02-18 13:35:35 -0500

    read and delete process

--- miami/lib/handler.js
+++ miami/lib/handler.js
@@ -2,8 +2,12 @@ let eList = require('../data/emails.json')
 // Import file system module
 const fs = require('fs')
 
+//Include the navigation 
+let navigation = require("../data/navigation.json")
+
+
 exports.newsletterSignup = (req,res) => {
-    res.render('newsletter-signup', { csrf : 'supersecret'  })
+    res.render('newsletter-signup', { csrf : 'supersecret', nav:navigation  })
  }
 
 exports.newsletterSignupProcess = (req,res) => {
@@ -31,6 +35,38 @@ exports.newsletterSignupProcess = (req,res) => {
  }
  
  exports.newsletterSignupList = (req,res) => {
+   let eList = require('../data/emails.json')
+
     console.log(eList)
-    res.render('userspage',{"users":eList.users})
+    res.render('userspage',{"users":eList.users, 
+       nav:navigation
+    })
+ }
+
+ exports.newsletterUser = (req,res) =>{
+   console.log(eList)
+   let userDetails = eList.users.filter((user)=>{
+      return user.email == req.params.email
+   })
+   console.log(userDetails)
+   res.render('userdetails',{"users": userDetails, nav:navigation})
+ }
+
+ exports.newsletterUserDelete = (req, res) => {
+   // Create a temporary list (object)
+   let newsList = {}
+   // retrieve all users filtering out the email we don't want
+   // != not equal
+    newsList.users = eList.users.filter((user)=>{
+      return user.email != req.params.email
+   })
+   console.log("Deleting "+ req.params.email)
+   // convert the object to a string before writing to the file
+   let json = JSON.stringify(newsList)
+
+    fs.writeFileSync('./data/emails.json',json,'utf-8',()=>{})
+
+    res.redirect(303,'/newsletter/list')
+
+
  }
--- miami/miami.js
+++ miami/miami.js
@@ -55,7 +55,11 @@ app.get('/basic',(req,res) => {
 app.get('/newsletter-signup', handler.newsletterSignup)
 app.post('/newsletter-signup/process', handler.newsletterSignupProcess)
 app.get('/newsletter/list',handler.newsletterSignupList)
-
+//Dynamic Routes
+//details shows one record 
+app.get('/newsletter/detail/:email',handler.newsletterUser)
+//delete users by email 
+app.get('/newsletter/delete/:email',handler.newsletterUserDelete)
 
 //error handling goes after the actual routes
 //The default response is not found
